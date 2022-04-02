62d399c299b05ccd3efbb640b337468e7636b0df
pbrocky <pbrcky@gmail.com>
2022-04-03 01:58:50 +0900

    Fixed p_setBlinkingIfCursor().

--- src/js/packageGlobal/blinkCursorFallback.js
+++ src/js/packageGlobal/blinkCursorFallback.js
@@ -1,4 +1,8 @@
-var p_BLINK_SVG = ( p_Trident === 9 || 2 <= p_Gecko ) && !p_canuseNativeForcedColors;
+/**
+ * Opera < 7.2 - アニメーション gif に非対応
+ * IE 9, Gecko 1.8.1+, Goanna - forced-colors 時に SVG を使うが animation 非対応なので js で点滅させる
+ */
+var p_BLINK_SVG = ( p_Trident === 9 || p_Gecko && 0 <= ua.conpare( p_engineVersion, '1.8.1' ) || p_Goanna ) && !p_canuseNativeForcedColors;
 
 var p_BLINKING_ELEMENTS = [];
 
@@ -28,8 +32,10 @@ var p_blinkElements = p_Presto < 7.2 ? function(){
 p_setBlinkingIfCursor = function ( elm ){
     if( p_blinkElements ){
         if( p_DOM_hasClassName( elm, 'pbChrCS' ) ){
-            if( !p_BLINKING_ELEMENTS.length && p_Presto ){
-                setInterval( p_blinkElements, 400 );
+            if( !p_blinkingTimerID ){
+                if( p_Presto || p_forcedColorsState ){
+                    p_blinkingTimerID = setInterval( /** @type {!Function} */ (p_blinkElements), 400 );
+                };
             };
             if( p_BLINKING_ELEMENTS.indexOf( elm ) === -1 ){
                 p_BLINKING_ELEMENTS.push( elm );
@@ -45,11 +51,21 @@ if( p_BLINK_SVG ){
         function( forcedColorsState ){
             if( forcedColorsState ){
                 if( !p_blinkingTimerID ){
-                    p_blinkingTimerID = setInterval( p_blinkElements, 400 );
+                    p_blinkingTimerID = setInterval( /** @type {!Function} */ (p_blinkElements), 400 );
                 };
-            } else {
+            } else if( p_blinkingTimerID ){
                 clearInterval( p_blinkingTimerID );
                 p_blinkingTimerID = 0;
+            };
+        }
+    );
+};
+
+if( p_blinkElements ){
+    p_listenUnloadEvent(
+        function(){
+            if( p_blinkingTimerID ){
+                clearInterval( p_blinkingTimerID );
             };
         }
     );
