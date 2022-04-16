dd3051fc4a328aaf373da4f4449bef0722f0fc8f
pbrocky <pbrcky@gmail.com>
2022-04-17 00:32:32 +0900

    Fixed forced color mode.

--- src/js/packageGlobal/forcedColorsMediaFeatureFallback.js
+++ src/js/packageGlobal/forcedColorsMediaFeatureFallback.js
@@ -3,18 +3,18 @@ if( !p_canuseNativeForcedColors ){
         function( forcedColorsState ){
             if( forcedColorsState ){
                 p_DOM_setAttribute(
-                    p_body,
+                    p_html,
                     'forced-colors',
                     forcedColorsState === 2 ? 'lod' :
                     forcedColorsState === 3 ? 'dol' : 'active'
                 );
 
                 if( p_cssTransformName || p_Trident === 9 || ( 5.5 <= p_Trident && p_Trident < 9 && p_iefilterEnabled ) ){
-                    p_DOM_addClassName( p_body, 'jsCanRotate' );
+                    p_DOM_addClassName( p_html, 'jsCanRotate' );
                 };
             } else {
-                p_DOM_removeAttribute( p_body, 'forced-colors' );
-                p_DOM_removeClassName( p_body, 'jsCanRotate' );
+                p_DOM_removeAttribute( p_html, 'forced-colors' );
+                p_DOM_removeClassName( p_html, 'jsCanRotate' );
             };
         }
     );
