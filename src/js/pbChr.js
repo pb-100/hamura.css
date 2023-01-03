ee13876f3551adff3bfe1685a24818f8f9e4ff1e
pbrocky <pbrcky@gmail.com>
2023-01-04 00:05:17 +0900

    Improved:pbChr.css on Gecko~1.8.1. pbKey on Gecko ~0.9.9.

--- src/js/pbChr.js
+++ src/js/pbChr.js
@@ -1,3 +1,26 @@
+if( p_Gecko < 1.9 ){
+    p_listenCssAvailabilityChange(
+        function( cssAvailability ){
+            if( cssAvailability ){
+                var elms = p_DOM_getElementsByTagNameFromDocument( '*' ),
+                    i = 0, l = elms.length, elm;
+    
+                for( ; i < l; ++i ){
+                    elm = elms[ i ];
+                    if( p_DOM_getTagName( elm ) !== 'B' && ( ' ' + elm.className ).toLowerCase().indexOf( ' pbchr' ) !== -1 ){
+                        if( !p_DOM_hasAttribute( elm, 'title' ) ){
+                            p_DOM_setAttribute( elm, 'title', elm.textContent );
+                        };
+                        elm.textContent = ' ';
+                    };
+                };
+
+                return true;
+            };
+        }
+    );
+};
+
 p_listenCssAvailabilityChange(
     function( cssAvailability ){
         if( cssAvailability ){
